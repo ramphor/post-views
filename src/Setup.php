444b67f0b9204884df3cf85ddec9b35c58f5a215
Puleeno Nguyen <puleeno@gmail.com>
2020-03-30 00:08:25 +0700

    Add new table to track history views

--- src/Setup.php
+++ src/Setup.php
@@ -7,13 +7,21 @@ class Setup
     {
         global $wpdb;
         $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ramphor_post_views(
-			ID BIGINT NOT NULL AUTO_INCREMENT,
-			user_id BIGINT NOT NULL DEFAULT 0,
-			post_id BIGINT NOT NULL,
-			views BIGINT NOT NULL DEFAULT 0,
-			last_views TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-			PRIMARY KEY(ID)
-		)";
+            ID BIGINT NOT NULL AUTO_INCREMENT,
+            user_id BIGINT NOT NULL DEFAULT 0,
+            post_id BIGINT NOT NULL,
+            views BIGINT NOT NULL DEFAULT 0,
+            last_views TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(ID)
+        )";
+        $wpdb->query($sql);
+
+        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ramphor_view_histories(
+            client_ip BIGINT NOT NULL DEFAULT 0,
+            post_id BIGINT NOT NULL,
+            last_views TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(client_ip)
+        )";
         $wpdb->query($sql);
     }
 }
