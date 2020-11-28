1f2e464fcd825546328a8836e08fa57551d9963b
Puleeno Nguyen <puleeno@gmail.com>
2020-11-28 21:10:59 +0700

    Change method track guest view

--- src/Handlers/UserHandler.php
+++ src/Handlers/UserHandler.php
@@ -6,8 +6,12 @@ use Ramphor\PostViews\Abstracts\HandlerAbstract;
 class UserHandler extends HandlerAbstract
 {
     protected $user_id;
+    protected $user_ip;
+    protected $expire_time;
+    protected $guest_expire_time;
 
     protected $enable_guest = false;
+    protected $track_guest_cookie = false;
     protected $tracking_history = false;
 
     public function __construct($enable_guest_user = false, $tracking_history = false)
@@ -23,6 +27,41 @@ class UserHandler extends HandlerAbstract
     public function enableGuest()
     {
         $this->enable_guest = true;
+    }
+
+    public function trackGuestCookie()
+    {
+        $this->track_guest_cookie = true;
+    }
+
+    public function setRemoteIP($ip)
+    {
+        $this->user_ip = $ip;
+    }
+
+    /**
+     * Expire time use to count user view.
+     * When the view has expire time it is a new view.
+     *
+     * @param int $seconds
+     */
+    public function setExpireTime($seconds = 86400)
+    {
+        $this->expire_time = $seconds;
+    }
+
+    /**
+     * Expire time use to count user view.
+     * When the view has expire time it is a new view.
+     *
+     * @param int $seconds
+     */
+    public function setGuestExpireTime($seconds = 86400, $track_guest_cookie = false)
+    {
+        $this->guest_expire_time = $seconds;
+        if ($track_guest_cookie) {
+            $this->trackGuestCookie();
+        }
     }
 
     public function enableTrackingViewHistory()
@@ -41,6 +80,5 @@ class UserHandler extends HandlerAbstract
         } else {
             $this->user_id = get_current_user_id();
         }
-
     }
 }
